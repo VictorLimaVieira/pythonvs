069534b6485ef3899430e7c44269b86fb113d686
VictorLimaVieira <victorlvminhaconta@gmail.com>
2025-08-20 10:10:44 -0300

    Atualizei com comando básicos

---
+++ apresentação/ap.py
@@ -0,0 +1,21 @@
+print('Olá, mundo!') # Imprime(exibe) mensagens ou resultados no terminal
+print(10+5)
+
+nome = "Victor"  # Variáveis
+idade = 22
+
+if idade >= 18:                 #Condiçoes
+    print('Maior de idade')
+else:
+    print("Menor de idade")
+
+for i in range(5):              # Laços de repeticão
+    print(i)
+
+def soma (a,b):               #Funções
+    return a + b
+
+print(soma(3,4))
+
+
+
---
+++ for.py
@@ -0,0 +1,10 @@
+from time import sleep
+for c in range(2,52,2):
+   print(c)
+   sleep(0.5)
+print('FIM')
+
+
+
+
+ 
